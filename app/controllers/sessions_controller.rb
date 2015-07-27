9fe85216e394e283380fa6b27ce1b3148479a3e8
diguage <leejun119@gmail.com>
2015-07-27 13:25:00 +0800

    添加登陆页面

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # TODO 登入用户，然后重定向到用户资料页
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 不完全正确
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
