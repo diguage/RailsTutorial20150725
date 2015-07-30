c63446090d4497f7b89e86fc65545fe45c6c5bf6
diguage <leejun119@gmail.com>
2015-07-30 22:36:17 +0800

    用户只允许修改自己

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -41,10 +42,17 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    # 确保用户已经登录
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in" # TODO 这里为什么不用flash.now呢？
         redirect_to login_url
       end
+    end
+
+    # 确保是正确用户
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
     end
 end
